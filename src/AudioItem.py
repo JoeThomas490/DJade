35db408e789b2a68c9449a34e9ebd037a6428c0b
JoeThomas490 <b4002810@my.shu.ac.uk>
2018-07-29 18:01:50 +0100

    Created start of the AudioItem class

---
+++ src/AudioItem.py
@@ -0,0 +1,7 @@
+class AudioItem:
+    def __init__(self,master):
+        self.source = StringVar()
+        self.fileName = StringVar()
+        self.artistTag = StringVar()
+        self.titleTag = StringVar()
+        self.genreTag = StringVar()
